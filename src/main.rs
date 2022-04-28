b567a1b6548387908d36f48c4099b21730289e63
Alex <alex.gyumushyan@gmail.com>
2022-04-28 16:20:39 -0400

    added window and some shapes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut window: PistonWindow = 
+        WindowSettings::new("Hello Piston!", [1280, 720])
+        .exit_on_esc(true).build().unwrap();
+
+
+    while let Some(event) = window.next() {
+        
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            
+            
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                    [0.0,0.0,100.0,100.0],
+                    context.transform,
+                    graphics);
+        
+            circle_arc([0.0, 1.0, 0.0, 1.0], 94.3, 0.0, 450.0, [500.0,500.0,100.0,100.0], context.transform, graphics);
+
+
+        });
+    }
+
+
+    //println!("Hello, world!");
 }
