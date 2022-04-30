5d4d3842aed964bb0c9c18bcaa9f6b001d175b03
Alex <alex.gyumushyan@gmail.com>
2022-04-29 21:18:52 -0400

    able to create objects on click that move with correct velocity

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-//use std::process;
+use piston::{MouseCursorEvent, MouseButton};
 use piston::window::WindowSettings;
 use piston::event_loop::{ EventSettings, Events };
 use piston::input::{Button, Key, PressEvent, RenderArgs, RenderEvent, UpdateEvent };
 use glutin_window::GlutinWindow;
 use opengl_graphics:: {GlGraphics, OpenGL };
 
-
 mod object;
+
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
 
 pub struct App {
     gl: GlGraphics,
     objects: Vec<object::Object>,
+    cursor_x: f64,
+    cursor_y: f64,
     x: i32,
     y: i32, 
     x_vel: i32,
@@ -29,31 +34,22 @@ impl App{
         const BACKGROUND: [f32; 4] = [62.0/255.0, 199.0/255.0, 230.0/255.0, 1.0];
         const FOREGROUND: [f32; 4] = [192.0/255.0, 27.0/255.0, 247.0/255.0, 1.0];
 
-        let main_rect = rectangle::square(0.0, 0.0, 50.0);
-        //let earth_rect = rectangle::square(0.0, 0.0, 50.0);
-
-
+        let main_rect = rectangle::square(0.0, 0.0, 20.0);
+        
         self.gl.draw(args.viewport(), |c, gl| {
 
             clear(BACKGROUND, gl);
+            
             rectangle(
                 FOREGROUND, 
                 main_rect, 
                 c.transform.trans(self.x as f64, self.y as f64), 
                 gl);
 
-            /*
-            rectangle(
-                FOREGROUND, 
-                earth_rect, 
-                c.transform.trans(earth.x_pos, earth.y_pos), 
-                gl);
-            */
-
             for obj in self.objects.iter() {
                 circle_arc(
                     FOREGROUND,
-                    53.0,
+                    20.0,
                     77.0,
                     47.0,
                     main_rect,
@@ -63,7 +59,23 @@ impl App{
         });
     }
 
+    fn update_cursor(&mut self, args: &[f64; 2]){
+        self.cursor_x = args[0];
+        self.cursor_y = args[1];
+    }
+
     fn update(&mut self) {
+        for i in 0..self.objects.len() {
+            self.objects[i].x_pos += self.objects[i].x_vel;
+            self.objects[i].y_pos += self.objects[i].y_vel;
+        } 
+
+        self.objects.retain(|elem| 
+            elem.x_pos < (WIDTH + 50) as f64 && 
+            elem.y_pos < (HEIGHT + 50) as f64 &&
+            elem.x_pos > -50.0 &&
+            elem.y_pos > -50.0);
+
         self.x += self.x_vel;
         self.y += self.y_vel;
     }
@@ -79,6 +91,26 @@ impl App{
                 _ => {}
             }
         }
+
+        if let &Button::Mouse(MouseButton) = args {
+            match MouseButton {
+                MouseButton::Left => {
+                    println!("This is a left click at x: {} and y: {}", self.cursor_x, self.cursor_y);
+                
+                    self.objects.push(
+                        object::Object::new(
+                            "Neptune".to_string(), 
+                            self.cursor_x, 
+                            self.cursor_y, 
+                            -1.0, 
+                            -1.0, 
+                            12311, 
+                        )
+                    );
+                }
+                _ => {}
+            }
+        }
     }
 }
 
@@ -87,11 +119,10 @@ fn main() {
     let opengl = OpenGL::V4_5;
 
 
-    let mut window: GlutinWindow = WindowSettings::new("Gravity Sim", [1280,720])
+    let mut window: GlutinWindow = WindowSettings::new("Gravity Sim", [WIDTH,HEIGHT])
         .exit_on_esc(true)
         .build()
         .unwrap();
-
 
     let earth = object::Object::new(
         "Earth".to_string(), 
@@ -109,9 +140,11 @@ fn main() {
         0.0, 
         123);
 
-    let mut app = App{
+    let mut app = App {
         gl: GlGraphics::new(opengl),
         objects: vec![earth, mars],
+        cursor_x: 0.0,
+        cursor_y: 0.0,
         x: 150,
         y: 300,
         x_vel: 0,
@@ -133,6 +166,9 @@ fn main() {
         if let Some(z) = e.press_args(){
             app.press(&z);
         }
-    }
 
+        if let Some(t) = e.mouse_cursor_args(){
+            app.update_cursor(&t);
+        }
+    }
 }
